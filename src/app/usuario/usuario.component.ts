07b6a225c21d94eebfeab380ad446f13cb7b6e4d
nicodiber <ndibernardo@gmail.com>
2024-06-11 18:39:00 -0300

    Update usuario.component.ts

--- src/app/usuario/usuario.component.ts
+++ src/app/usuario/usuario.component.ts
@@ -11,7 +11,15 @@ import { User } from './user'; // Importa la interfaz User
   styleUrl: './usuario.component.css' // La ruta del archivo CSS de este componente
 })
 export class UsuarioComponent implements OnInit {
-  usuarios: User[] = []; // Declara la propiedad usuarios como un array vacío de objetos User
+  usuarios: User[] = [
+    //simulamos usuarios para testear
+    usuarios: User[] = [
+      new User(1, 'nico', 'dib', 'ndib@gmail.com'),
+      new User(2, 'ale', 'teb', 'ateb@gmail.com'),
+      new User(3, 'bruno', 'nei', 'bnei@gmail.com'),
+      new User(4, 'eli', 'mir', 'emir@gmail.com')
+    ];
+  ]; // Declara la propiedad usuarios como un array vacío de objetos User
 
   // El constructor inyecta el servicio UsuarioService, asignándolo a la propiedad privada _userService
   constructor(
@@ -20,11 +28,12 @@ export class UsuarioComponent implements OnInit {
 
   // Método del ciclo de vida de Angular que se ejecuta después de la creación del componente
   ngOnInit() {
+    //this.getUsers();
   }
 
   // Método para obtener la lista de usuarios usando el servicio UsuarioService
   getUsers() {
-    this._userService.getUsers()
-      .then(usuarios => this.usuarios = usuarios) // Asigna la lista de usuarios obtenida a la propiedad usuarios
+    //this._userService.getUsers()
+    //.then(usuarios => this.usuarios = usuarios) // Asigna la lista de usuarios obtenida a la propiedad usuarios
   }
 }
